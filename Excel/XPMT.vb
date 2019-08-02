b9f178eb37bb86616ca16af0ebae03e4eb0a8a88
Ahab <52932572+JeanRabelo@users.noreply.github.com>
2019-08-02 10:35:11 -0300

    Update XPMT.vb
    Código melhorado para funcionar não só com datas, mas com números inteiros também (fora a melhoria de ignorar células em branco).

--- Excel/XPMT.vb
+++ Excel/XPMT.vb
@@ -2,12 +2,14 @@ Function XPMT(value As Double, tx360 As Double, dates As Range)
     
     Dim i, j, n, m As Integer
     Dim tx1 As Double
+    Dim celula_relevante As Boolean
     
     tx1 = (1 + tx360) ^ (1 / 360) - 1
     n = dates.Count
     m = 0
     For i = 1 To n
-        If IsDate(dates(i).value) Then
+        celula_relevante = (IsDate(dates(i).value) Or IsNumeric(dates(i).value)) And (Not IsEmpty(dates(i).value))
+        If celula_relevante Then
             m = m + 1
         End If
     Next i
@@ -18,7 +20,8 @@ Function XPMT(value As Double, tx360 As Double, dates As Range)
     
     j = 0
     For i = 1 To n
-        If IsDate(dates(i).value) Then
+        celula_relevante = (IsDate(dates(i).value) Or IsNumeric(dates(i).value)) And (Not IsEmpty(dates(i).value))
+        If celula_relevante Then
             truedates(j) = dates(i).value
             j = j + 1
         End If
