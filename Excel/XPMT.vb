6296883b7b3b8b89c34b25da7157aeecb5967437
Ahab <52932572+JeanRabelo@users.noreply.github.com>
2019-08-02 10:22:35 -0300

    Update XPMT.vb
    Agora o XPMT leva leva em consideração apenas o que for "data" no range de datas.

--- Excel/XPMT.vb
+++ Excel/XPMT.vb
@@ -1,16 +1,34 @@
 Function XPMT(value As Double, tx360 As Double, dates As Range)
     
-    Dim i, n As Integer
+    Dim i, j, n, m As Integer
     Dim tx1 As Double
     
     tx1 = (1 + tx360) ^ (1 / 360) - 1
     n = dates.Count
+    m = 0
+    For i = 1 To n
+        If IsDate(dates(i).value) Then
+            m = m + 1
+        End If
+    Next i
     
-    ReDim dias(n - 1) As Integer
-    ReDim mult(n - 1) As Double
+    'Agora m é a quantidade de dias válidos
     
-    For i = 0 To n - 1
-        dias(i) = dates(i + 1).value - dates(1).value
+    ReDim truedates(m - 1) As Date
+    
+    j = 0
+    For i = 1 To n
+        If IsDate(dates(i).value) Then
+            truedates(j) = dates(i).value
+            j = j + 1
+        End If
+    Next i
+    
+    ReDim dias(m - 1) As Integer
+    ReDim mult(m - 1) As Double
+    
+    For i = 0 To m - 1
+        dias(i) = truedates(i) - truedates(0)
         mult(i) = 1 / (1 + tx1) ^ (dias(i))
     Next i
     
