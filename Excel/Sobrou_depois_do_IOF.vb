36c85b35107af020c99485b40c3a2a5e37705cc0
Ahab <52932572+JeanRabelo@users.noreply.github.com>
2019-08-15 11:16:41 -0300

    Update Sobrou_depois_do_IOF.vb

--- Excel/Sobrou_depois_do_IOF.vb
+++ Excel/Sobrou_depois_do_IOF.vb
@@ -1,9 +1,11 @@
 Function Sobrou_depois_do_IOF(d_i As Integer)
-    
     Dim Wsf As Object
+    Dim Porcentagem As Double
     
     Set Wsf = Application.WorksheetFunction
     
-    Sobrou_depois_do_IOF = (4 / 100) * d_i - (1 / 100) * (Wsf.Quotient(d_i, 3) * 2 + Wsf.GeStep((d_i - 1) Mod 3, 3) * Wsf.GeStep(d_i Mod 3, 3))
-
+    Porcentagem = (4 / 100) * d_i - (1 / 100) * (Wsf.Quotient(d_i, 3) * 2 + Wsf.GeStep(((d_i - 1) Mod 3), 1) * Wsf.GeStep((d_i Mod 3), 1))
+    
+    Sobrou_depois_do_IOF = Wsf.Min(Wsf.Max(Porcentagem, 0), 1)
+    
 End Function
