42ac47f9c42c126b4572f6f6faa704740240b51b
Ahab <52932572+JeanRabelo@users.noreply.github.com>
2019-10-29 16:23:38 -0300

    tentativa_1

--- Operacional/cadastro_positivo/programas/criar_arvore/criar_metaarvore_005.py
+++ Operacional/cadastro_positivo/programas/criar_arvore/criar_metaarvore_005.py
@@ -2,63 +2,108 @@
 
 
 class Historico:
+
     def __init__(self):
+
+        class Cliente:
+            class Operacao:
+                class DetalheOperacao:
+                    def __init__(self):
+                        self.nome_reduzido = 'DetOpr'
+                        self.atributos = {'InPreFix' : '',
+                        'DtVnctUltPcl' : '',
+                        'VlCtrdFut' : '',
+                        'QtPcl' : '',
+                        'NrPlstCrt' : '',
+                        'SdoDvdr' : '',
+                        'DtVnctOpr' : '',
+                        'NrGr' : '',
+                        'CdCt' : '',
+                        'SeqCt' : '',
+                        'SitCt' : '',
+                        'VlObgc' : '',
+                        'DtVnctUltPclCsr' : '',
+                        'QtPclCtCsr' : '',
+                        'SdoDvdrCtCsr' : '',
+                        'DtContCtCsr' : ''}
+
+                class ParcelaAnterior:
+                    class PagamentoParcelaAnterior:
+                        def __init__(self):
+                            self.nome_reduzido = 'PgtoPclAnt'
+                            self.atributos = {'DtPgtoPclAnt' : '',
+                            'VlPgtoPclAnt' : ''}
+
+                    def __init__(self):
+                        self.nome_reduzido = 'PclAnt'
+                        self.atributos = {'DtVnctPclAnt' : '',
+                        'PercPclAnt' : '',
+                        'VlPclAnt' : '',
+                        'CmdoPclAnt' : ''}
+
+                        self.pagamentos_parcela_anterior_min = 0
+                        self.pagamentos_parcela_anterior_max = 'ilimitado'
+                        self.pagamentos_parcela_anterior = list()
+
+                class PagamentoAvulso:
+                    def __init__(self):
+                        self.nome_reduzido = 'PgtoAvls'
+                        self.atributos = {'DtPgtoAvls' : '',
+                        'VlPgtoAvls' : '',
+                        'TipPgtoAvls' : '',
+                        'CmdoPgtoAvls' : ''}
+
+                def __init__(self):
+                    self.nome_reduzido = 'Opr'
+                    self.atributos = {'NrUnco' : '',
+                    'PrfAg' : '',
+                    'NrCtr' : '',
+                    'DtCtrc' : '',
+                    'CdMdld' : '',
+                    'DtAprc' : '',
+                    'CnpjCtrc' : '',
+                    'CmdoOpr' : '',
+                    'IdfcCliCtrt' : '',
+                    'TipRst' : ''}
+
+                    self.parcelas_anteriores_min = 0
+                    self.parcelas_anteriores_max = 'ilimitado'
+
+                    self.pagamentos_avulsos_min = 0
+                    self.pagamentos_avulsos_max = 'ilimitado'
+
+                    self.parcelas_futuras_min = 0
+                    self.parcelas_futuras_max = 'ilimitado'
+
+            def __init__(self):
+                self.nome_reduzido = 'Cli'
+                self.atributos = {'TipCli' : '',
+                'IdfcCli' : '',
+                'NmCli' : '',
+                'NtzRlc' : '',
+                'CdOcr' : '',
+                'CmdoCli' : ''}
+
+                self.operacoes_min = 0
+                self.operacoes_max = 'ilimitado'
+
+
         self.nome_reduzido = 'EnvoHstCrd'
-        self.atributos = dict()
+        self.atributos = {'CnpjIf' : '',
+        'CnpjGbd' : '',
+        'CmdoRms' : '',
+        'NrRms' : '',
+        'SeqlRms' : '',
+        'DtRms' : '',
+        'DtRefBaseItno' : '',
+        'NrRmsRlc' : '',
+        'FmtRms' : ''}
 
         self.clientes_min = 0
         self.clientes_max = 'ilimitado'
-        self.clientes = list()
+        self.clientes = Cliente
 
-class Cliente:
-    def __init__(self):
-        self.nome_reduzido = 'Cli'
-        self.atributos = dict()
 
-        self.operacoes_min = 0
-        self.operacoes_max = 'ilimitado'
-        self.operacoes = list()
+historico = Historico
 
-class Operacao:
-    def __init__(self):
-        self.nome_reduzido = 'Opr'
-        self.atributos = dict()
-
-        self.detalhe_operacoes_min = 0
-        self.detalhe_operacoes_max = 1
-        self.detalhe_operacoes = dict()
-
-        self.parcelas_anteriores_min = 0
-        self.parcelas_anteriores_max = 'ilimitado'
-        self.parcelas_anteriores = list()
-
-        self.pagamentos_avulsos_min = 0
-        self.pagamentos_avulsos_max = 'ilimitado'
-        self.pagamentos_avulsos = list()
-
-        self.parcelas_futuras_min = 0
-        self.parcelas_futuras_max = 'ilimitado'
-        self.parcelas_futuras = list()
-
-class DetalheOperacao:
-    def __init__(self):
-        self.nome_reduzido = 'DetOpr'
-        self.atributos = dict()
-
-class ParcelaAnterior:
-    def __init__(self):
-        self.nome_reduzido = 'PclAnt'
-        self.atributos = dict()
-        self.pagamentos_parcela_anterior_min = 0
-        self.pagamentos_parcela_anterior_max = 'ilimitado'
-        self.pagamentos_parcela_anterior = list()
-
-class PagamentoParcelaAnterior:
-    def __init__(self):
-        self.nome_reduzido = 'PgtoPclAnt'
-        self.atributos = dict()
-
-class PagamentoAvulso:
-    def __init__(self):
-        self.nome_reduzido = 'PgtoAvls'
-        self.atributos = dict()
+print(historico().atributos)
